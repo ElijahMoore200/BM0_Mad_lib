3eb6fc9939a0dbd82a62c5e0f71679c439004c62
ElijahMoore200 <ELijahm390@nycstudents.net>
2025-11-25 11:48:30 -0500

    Update BM0_Mad_lib.py

--- BM0_Mad_lib.py
+++ BM0_Mad_lib.py
@@ -4,6 +4,8 @@ print(f"Today, me and my freinds are having a {noun} party")
 time.sleep(2)
 possesive_noun = input("We're going to ____(possesive_noun) house:")
 print(f"We're going to {possesive_noun} house")
+plural noun = input("We're planning to float on ____(pluarl noun)")
+print(f"We're planning to float on  {pluarl noun}")
 
 
 
