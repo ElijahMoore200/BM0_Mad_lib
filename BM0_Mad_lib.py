8c88a050ea262372f256c3892793b6e5f66bd441
ElijahMoore200 <ELijahm390@nycstudents.net>
2025-11-25 11:44:06 -0500

    Update BM0_Mad_lib.py

--- BM0_Mad_lib.py
+++ BM0_Mad_lib.py
@@ -1,8 +1,8 @@
 import time 
 noun = input("Today, me and my freinds are having a ___ party! (noun):")
-possesive_noun = input("We're going to ____(possesive_noun):")
 print(f"Today, me and my freinds are having a {noun} party")
 time.sleep(5)
+possesive_noun = input("We're going to ____(possesive_noun):")
 print(f"We're going to {possesive_noun} house")
 
 
