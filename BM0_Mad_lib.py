c75aa8642f1bee957c6ce77a58417f3e54966311
ElijahMoore200 <ELijahm390@nycstudents.net>
2025-12-01 10:20:11 -0500

    Update BM0_Mad_lib.py

--- BM0_Mad_lib.py
+++ BM0_Mad_lib.py
@@ -1,24 +1,24 @@
 import time 
 import random
 choice = random.radient(1,2)
-	if choice == 1:
-noun = input("Today, me and my freinds are having a ___ party! (noun):")
-print(f"Today, me and my freinds are having a {noun} party")
-time.sleep(2)
-possesive_noun = input("We're going to ____(possesive_noun) house:")
-print(f"We're going to {possesive_noun} house")
-time.sleep(2)
-plural_noun = input("We're planning to float on ____(plural_noun):")
-print(f"We're planning to float on  {plural_noun}")
-time.sleep(2)
-noun2 = input("We'll go down the water___(noun):")
-print(f"We'll go down the water {noun2}")
-	if choice == 2
-adjective = input("I always thought he was___(adjective):")
-print(f"I always thought he was {adjective}")
-time.sleep(2)
-plural_noun2 = input("He went all the way to ____(plural_noun) ")
-print(f"He went all the way to {plural_noun2}")
+if choice == 1:
+	noun = input("Today, me and my freinds are having a ___ party! (noun):")
+	print(f"Today, me and my freinds are having a {noun} party")
+	time.sleep(2)
+	possesive_noun = input("We're going to ____(possesive_noun) house:")
+	print(f"We're going to {possesive_noun} house")
+	time.sleep(2)
+	plural_noun = input("We're planning to float on ____(plural_noun):")
+	print(f"We're planning to float on  {plural_noun}")
+	time.sleep(2)
+	noun2 = input("We'll go down the water___(noun):")
+	print(f"We'll go down the water {noun2}")
+if choice == 2
+	adjective = input("I always thought he was___(adjective):")
+	print(f"I always thought he was {adjective}")
+	time.sleep(2)
+	plural_noun2 = input("He went all the way to ____(plural_noun) ")
+	print(f"He went all the way to {plural_noun2}")
 
 
 
