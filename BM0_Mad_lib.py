4360908715a9e7414a5cfb644a0cca3342315fab
ElijahMoore200 <ELijahm390@nycstudents.net>
2025-12-01 10:19:11 -0500

    Update BM0_Mad_lib.py

--- BM0_Mad_lib.py
+++ BM0_Mad_lib.py
@@ -1,7 +1,7 @@
 import time 
 import random
 choice = random.radient(1,2)
-if choice == 1:
+	if choice == 1:
 noun = input("Today, me and my freinds are having a ___ party! (noun):")
 print(f"Today, me and my freinds are having a {noun} party")
 time.sleep(2)
@@ -13,7 +13,7 @@ print(f"We're planning to float on  {plural_noun}")
 time.sleep(2)
 noun2 = input("We'll go down the water___(noun):")
 print(f"We'll go down the water {noun2}")
-if choice == 2
+	if choice == 2
 adjective = input("I always thought he was___(adjective):")
 print(f"I always thought he was {adjective}")
 time.sleep(2)
