af4c8865516b71592f70ef50869eed9e246a7b22
ElijahMoore200 <ELijahm390@nycstudents.net>
2025-12-05 09:29:19 -0500

    Update stoplight.py

--- stoplight.py
+++ stoplight.py
@@ -2,7 +2,7 @@ from tkinter import *
 #need to install on all machines
 from tkmacosx import Button
 import tkinter as tk 
-T = Text(root, height = 5, width = 52)
+
 # Create the main window
 root = Tk()
 root.title("Enter Title Here")
@@ -15,10 +15,10 @@ red_button = Button(root, text="Red", background='red')
 yellow_button = Button(root,text="Yellow", background='yellow')
 green_button= Button(root,text='Green',background='green')
 #Add a label
-label = Label(root, text="This is a stoplight")
+label = Label(root, text = "This is a stoplight")
 textarea = tk.Text(root, width = 50, height = 10, wrap="word")
 # Place widgets in window (with pack function!)
-
+T = Text(root, height = 5, width = 52)
 red_button.pack()
 yellow_button.pack()
 green_button.pack()
